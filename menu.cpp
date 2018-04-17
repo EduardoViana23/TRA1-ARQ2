6af6333267bfe66a3069c16a51c3c9f956dfc925
EduardoViana23 <38244652+EduardoViana23@users.noreply.github.com>
2018-04-17 19:05:38 -0300

    Add files via upload

--- menu.cpp
+++ menu.cpp
@@ -6,19 +6,47 @@ Menu::Menu(QWidget *parent) :
     ui(new Ui::Menu)
 {
     ui->setupUi(this);
+    ui->check_forwarding->setChecked(false);
+    ui->check_forwarding->setVisible(false);
+    ui->radio_in_in->setChecked(false);
+    ui->radio_in_out->setChecked(false);
+    ui->radio_out_out->setChecked(true);
+    in_in=in_out=forwarding=false;
+    out_out=true;
+
+    ui->spinBox_if->setRange(1,100);
+    ui->spinBox_id->setRange(1,100);
+    ui->spinBox_ex->setRange(1,100);
+    ui->spinBox_w->setRange(1,100);
+
+    std::list<Instrucao> lista_i=this->listagem("a.txt");
+    std::list<Instrucao>::iterator it=lista_i.begin();
+    Instrucao aux;
+    while(it!=lista_i.end())
+    {
+
+        aux=*it;
+        lista.insert(std::pair<int,Instrucao>(aux.getId(),aux));
+
+    }
 }
 
 std::list<Instrucao> Menu::listagem(QString file)
 {
-    try{
+
     std::list<Instrucao> lista ;
 
         std::ifstream arquivo;
         arquivo.open(file.toStdString().c_str(),std::ios::in);
         if (!arquivo.is_open())
         {
-            arquivo.close();
-            throw QString("Erro, arquivo de instruções não encontrado!");
+            std::ofstream arquivoa(file.toStdString());
+            if (!arquivoa.is_open())
+            {
+               arquivoa.close();
+
+            }
+
         }
         std::string linha;
 
@@ -35,10 +63,7 @@ std::list<Instrucao> Menu::listagem(QString file)
 
            return lista;
 
-    }
-    catch(QString &erro){
-        throw erro;
-    }
+
 
 
 
@@ -47,4 +72,75 @@ std::list<Instrucao> Menu::listagem(QString file)
 Menu::~Menu()
 {
     delete ui;
+
 }
+
+
+
+void Menu::on_radio_in_in_clicked(bool checked)
+{
+    ui->check_forwarding->setVisible(checked);
+    in_in=checked;
+}
+
+void Menu::on_radio_in_out_clicked(bool checked)
+{
+   ui->check_forwarding->setVisible(checked);
+    in_in=checked;
+}
+
+
+void Menu::on_radio_out_out_clicked(bool checked)
+{
+   ui->check_forwarding->setVisible(!checked);
+    in_in=checked;
+
+}
+
+
+void Menu::on_check_forwarding_toggled(bool checked)
+{
+    forwarding=checked;
+}
+
+void Menu::on_Button_gera_clicked()
+{
+
+    janela_de_resutado.clear_tables();
+
+    if(in_in)//Iron
+    {
+        if(forwarding)
+        {
+            //janela_de_resutado.set();
+        }
+       else
+        {
+            //janela_de_resutado.set();
+        }
+
+    }
+    else if(in_out)//Eduardo
+    {
+
+    }
+    else//Aurélio
+    {
+
+            //janela_de_resutado.set();
+
+    }
+
+     //janela_de_resutado.setModal(true);
+     this->hide();
+     janela_de_resutado.exec();
+     this->show();
+}
+
+//void Menu::Out_Out(std::map<int, Instrucao> A, int n_if, int n_id, int n_ex, int n_if )
+//{
+   //std::vector<vector<int>>
+
+
+
+//}
